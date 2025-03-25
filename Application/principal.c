40555be6329e67b78dc9d563fad764573208abc2
Bongibault Romain <bongibault.romain@gmail.com>
2025-03-25 09:51:52 +0100

    TP1 fin

--- Application/principal.c
+++ Application/principal.c
@@ -9,17 +9,19 @@
 uint32_t *stack1 = (uint32_t *) STACK_1_ADDRESS;
 uint32_t *stack2 = (uint32_t *) STACK_2_ADDRESS;
 
+#include <stdio.h>
+
 char current_process = 0;
 
-char setup = 0;
+//char setup = 0;
 
  void SysTick_Handler(void) {
-	if (!setup){
-		__set_PSP((uint32_t)stack1);
-		setup = 1;
+//	if (!setup){
+//		__set_PSP((uint32_t)stack1);
+//		setup = 1;
 //		SCB->ICSR |= SCB_ICSR_PENDSVSET ;
-		return;
-	}
+//		return;
+//	}
 	
 	if(current_process){
 		stack2 = (uint32_t *) __get_PSP();
@@ -30,22 +32,28 @@ char setup = 0;
 	}
 	
 	current_process = !current_process;
-//	SCB->ICSR |= SCB_ICSR_PENDSVSET ;
+	SCB->ICSR |= SCB_ICSR_PENDSVSET ;
 	return;
 }
 
-//void PendSV_Handler(){
-//	
-//}
+void PendSV_Handler(){
+	uint32_t *previous_stack = current_process ? stack1 : stack2;
+	uint32_t *next_stack = current_process ? stack2 : stack1;
+	
+  __ASM volatile ("MOV %0, r4"  : "=r" (previous_stack[8]));
+	__ASM volatile ("MOV r4, %0" : : "r" (next_stack[8]) : );
+}
 
 void dummy1(void) {
+		int a = 0;
 	while(1){
-		int a = 2;
 		a++;
+		__ASM volatile ("MOV r4, %0" : : "r" (a) : );
 	}
 }
 
 void dummy2(void) {
+	__ASM volatile ("MOV r4, #15" );
 	while(1){
 		int a = 2;
 		a++;
@@ -71,14 +79,19 @@ int main ( void )
 	SysTick->CTRL |= SysTick_CTRL_ENABLE + SysTick_CTRL_TICKINT + SysTick_CTRL_CLKSOURCE;
 	SysTick->LOAD |= 72000;
 	
-//	NVIC_SetPriority(SysTick_IRQn, 0x0);
-//	NVIC_SetPriority(PendSV_IRQn, 0x3);
+	NVIC_SetPriority(SysTick_IRQn, 0x0);
+	NVIC_SetPriority(PendSV_IRQn, 0x3);
 	initialize_stacks();
+	
+	
+	__set_PSP((uint32_t)stack1);
 	
 	uint32_t ctrl = __get_CONTROL();
 	ctrl |= 3;
 	__set_CONTROL(ctrl);
 	
+	dummy1();
+
 	while (1)
 	{
 	}
